5b9689dd609a421c96705df346b54600dc66090b
panosbakk <panosbakaloudis@gmail.com>
2024-02-23 18:40:47 +0200

    initial commit

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+    clerkId: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    photo: {
+        type: String,
+        required: true
+    },
+    firstName: { type: String },
+    lastName: { type: String },
+    planId: {
+        type: String,
+        default: 1
+    },
+    creditBalance: {
+        type: Number,
+        default: 10
+    },
+})
+
+const User = models?.Image || model('User', UserSchema)
+export default User;
