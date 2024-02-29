4b33f995f80b706aaa1e8a1657aef56b72861484
panosbakk <panosbakaloudis@gmail.com>
2024-02-29 13:31:42 +0200

    fix: small fix

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -3,10 +3,15 @@ import TransformationForm from '@/components/shared/TransformationForm';
 import { transformationTypes } from '@/constants'
 import { getUserById } from '@/lib/actions/user.actions';
 import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
 const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
   const { userId } = auth();
   const transformation = transformationTypes[type];
+
+  if(!userId){
+    redirect('/sign-in')
+  }
 
   const user = await getUserById(userId)
 
