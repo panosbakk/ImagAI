03cd60c87378ea43c8ee62f87fea46920c621a82
panosbakk <panosbakaloudis@gmail.com>
2024-03-03 13:39:33 +0200

    feat: implemented alert modal for when credits are insufficient

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -26,7 +26,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { aspectRatioOptions, creditFee, defaultValues, transformationTypes } from "@/constants"
 import { CustomField } from "./CustomField"
-import { useState, useTransition } from "react"
+import { useEffect, useState, useTransition } from "react"
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
 import { updateCredits } from "@/lib/actions/user.actions"
 import MediaUploader from "./MediaUploader"
@@ -34,6 +34,7 @@ import TransformedImage from "./TransformedImage"
 import { getCldImageUrl } from "next-cloudinary"
 import { addImage, updateImage } from "@/lib/actions/image.actions"
 import { useRouter } from "next/navigation"
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal"
 
 export const formSchema = z.object({
     title: z.string(),
@@ -170,9 +171,16 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
         })
     }
 
+    useEffect(() => {
+        if(image && (type === 'restore' || type === 'removeBackground')){
+            setNewTransformation(transformationType.config)
+        }
+    }, [image, transformationType.config, type])
+
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
                 <CustomField
                     control={form.control}
                     name="title"
